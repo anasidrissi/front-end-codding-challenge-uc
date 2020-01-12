6ad457107f89c461efe060e8027460cf02fb18d9
Idrissi Anas <anasidrissi19@gmail.com>
2020-01-12 20:09:06 +0100

    Add repository row component
    this create angular app and add bootstrap 4

---
+++ src/app/repository/repository.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository',
+  templateUrl: './repository.component.html',
+  
+})
+export class RepositoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
